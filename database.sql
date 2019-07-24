ca20a494c68c0f7f9ab98744962c299622b1b170
ahmadiesa-abu <53178237+ahmadiesa-abu@users.noreply.github.com>
2019-07-24 16:08:35 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,7 @@
 CREATE DATABASE pools;
+create user pool_editor with encrypted password 'poolEdit12';
+grant all privileges on database pools to pool_editor;
+#then connect to database using the following command
+psql -U pool_editor pools
 create table pool(id varchar(255) PRIMARY KEY,name varchar(255));
 create table resource(pool_id varchar(255),id varchar(255) PRIMARY KEY,ip_address varchar(255),status varchar(255));
