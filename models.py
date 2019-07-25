0e82b6881f711bba8009f4345ad11ed9df292372
ahmadiesa-abu <53178237+ahmadiesa-abu@users.noreply.github.com>
2019-07-25 17:05:16 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ class Pool(db.Model):
 
     id = db.Column(db.String, primary_key=True)
     name = db.Column(db.String())
+    resources = db.relationship('Resource', backref='Pool', lazy=True)
     
     def __init__(self, id, name):
         self.id = id
@@ -15,13 +16,15 @@ class Pool(db.Model):
     
     def serialize(self):
         return {
-            'name': self.name
+            'name': self.name,
+	    'resources' : self.resources
         }
 		
 class Resource(db.Model):
     __tablename__ = 'resource'
 	
-    pool_id = db.Column(db.String())
+    pool_id = db.Column(db.String(),db.ForeignKey('Pool.id'),
+        nullable=False)
     id = db.Column(db.String, primary_key=True)
     ip_address = db.Column(db.String())
     status = db.Column(db.String())
